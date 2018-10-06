e8c6a929d9814818ba23b897da7650f39b93a019
Gennadiy Falatiuk <gena.fox.gf@gmail.com>
2018-10-06 11:18:43 +0300

    C# training project added.

---
+++ C#/Training/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Training
+{
+    class Program
+    {
+        private static int[] Numbers = { 4, 8, 15, 16, 23, 42, 108 };
+
+        static void Main(string[] args)
+        {
+            LinqQuerySyntax();
+        }
+
+        private static void LinqQuerySyntax() 
+        {
+            var even = 
+                from n in Numbers
+                where n % 2 == 0
+                orderby n descending
+                select n;
+
+            Console.WriteLine("----- LINQ Query Syntax -----");
+            Console.WriteLine(string.Join(", ", even));
+            Console.WriteLine();
+        }
+    }
+}
